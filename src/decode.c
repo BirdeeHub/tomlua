0e06cab4b2de4915956a19aa38fe5159596bf73b
BirdeeHub <birdee@localhost>
2025-08-30 09:03:41 -0700

    Renamed my cat to Blob because he likes to blob around so much
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/decode.c

--- src/decode.c
+++ src/decode.c
@@ -20,31 +20,27 @@ static bool heading_nav(lua_State *L, keys_result *keys, bool array_type, int to
     lua_rawgeti(L, LUA_REGISTRYINDEX, top);
     for (size_t i = 0; i < keys->len; i++) {
         if (!push_buf_to_lua_string(L, &keys->v[i])) {
-            set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-            return false;
+            return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
         }
         lua_gettable(L, -2); // get t[key]
         if (lua_isnil(L, -1)) {
             lua_pop(L, 1); // remove non-table
             lua_newtable(L); // create table
             if (!push_buf_to_lua_string(L, &keys->v[i])) {
-                set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-                return false;
+                return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
             }
             lua_pushvalue(L, -2); // push new table
             lua_settable(L, -4); // t[key] = new table
         } else if (!lua_istable(L, -1)) {
             lua_pop(L, 1); // remove non-table
-            set_err_upval(L, false, 33, "cannot navigate through non-table");
-            return false;
+            return set_err_upval(L, false, 33, "cannot navigate through non-table");
         }
         lua_remove(L, -2); // remove parent table, keep child on top
     }
     if (array_type) {
         // We’re at the table that should act as an array
         if (!lua_istable(L, -1)) {
-            set_err_upval(L, false, 37, "target of array heading isn't a table");
-            return false;
+            return set_err_upval(L, false, 37, "target of array heading isn't a table");
         }
 #if LUA_VERSION_NUM == 501
         size_t len = lua_objlen(L, -1);
@@ -67,8 +63,7 @@ static bool heading_nav(lua_State *L, keys_result *keys, bool array_type, int to
 static bool set_kv(lua_State *L, keys_result *keys) {
     if (!keys->ok) { return false; }
     if (keys->len <= 0) {
-        set_err_upval(L, false, 22, "no key provided to set");
-        return false;
+        return set_err_upval(L, false, 22, "no key provided to set");
     }
     int value_idx = lua_gettop(L);  // value is on top
     int root_idx = value_idx - 1;   // the table to start navigation from
@@ -77,31 +72,27 @@ static bool set_kv(lua_State *L, keys_result *keys) {
     // Navigate through all keys except the last
     for (size_t i = 0; i < keys->len - 1; i++) {
         if (!push_buf_to_lua_string(L, &keys->v[i])) {
-            set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-            return false;
+            return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
         }
         lua_gettable(L, -2);                        // get t[key]
         if (lua_isnil(L, -1)) {
             lua_pop(L, 1);      // remove nil
             lua_newtable(L);    // create new table
             if (!push_buf_to_lua_string(L, &keys->v[i])) {
-                set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-                return false;
+                return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
             }
             lua_pushvalue(L, -2); // push new table
             lua_settable(L, -4);  // t[key] = new table
         } else if (!lua_istable(L, -1)) {
             lua_pop(L, 2);
-            set_err_upval(L, false, 18, "key is not a table");
-            return false;
+            return set_err_upval(L, false, 18, "key is not a table");
         }
         lua_remove(L, -2); // remove parent table
     }
 
     // Now top table is where we want to set the value
     if (!push_buf_to_lua_string(L, &keys->v[keys->len - 1])) {
-        set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-        return false;
+        return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
     }
     lua_pushvalue(L, value_idx); // push value
     lua_settable(L, -3);         // t[last_key] = value
