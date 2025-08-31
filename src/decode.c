e054b78849f63febad62b5eb969e9e9d1ef9dc1c
BirdeeHub <birdee@localhost>
2025-08-31 03:10:37 -0700

    Reimplemented the quantum foobulator to enhance its grokking abilities
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/decode.c

--- src/decode.c
+++ src/decode.c
@@ -7,6 +7,52 @@
 #include "parse_keys.h"
 #include "parse_val.h"
 
+// TODO: make this strict!
+static bool heading_nav_strict(lua_State *L, keys_result *keys, bool array_type, int top) {
+    if (!keys->ok) return false;
+    if (keys->len <= 0) return false;
+    lua_rawgeti(L, LUA_REGISTRYINDEX, top);
+    for (size_t i = 0; i < keys->len; i++) {
+        if (!push_buf_to_lua_string(L, &keys->v[i])) {
+            return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
+        }
+        lua_gettable(L, -2);  // get t[key]
+        if (lua_isnil(L, -1)) {
+            lua_pop(L, 1);  // remove non-table
+            lua_newtable(L);  // create table
+            if (!push_buf_to_lua_string(L, &keys->v[i])) {
+                return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
+            }
+            lua_pushvalue(L, -2);  // push new table
+            lua_settable(L, -4);   // t[key] = new table
+        } else if (!lua_istable(L, -1)) {
+            lua_pop(L, 1);  // remove non-table
+            return set_err_upval(L, false, 33, "cannot navigate through non-table");
+        }
+        lua_remove(L, -2);  // remove parent table, keep child on top
+    }
+    if (array_type) {
+        // We’re at the table that should act as an array
+        if (!lua_istable(L, -1)) {
+            return set_err_upval(L, false, 37, "target of array heading isn't a table");
+        }
+#if LUA_VERSION_NUM == 501
+        size_t len = lua_objlen(L, -1);
+#else
+        size_t len = lua_rawlen(L, -1);
+#endif
+        // append new table at len+1
+        lua_newtable(L);               // new element
+        lua_pushinteger(L, len + 1);
+        lua_pushvalue(L, -2);          // copy new element
+        lua_settable(L, -4);           // t[len+1] = new element
+
+        // remove parent array table, leave new element on top
+        lua_remove(L, -2);
+    }
+    return true;
+}
+
 static bool heading_nav(lua_State *L, keys_result *keys, bool array_type, int top) {
     if (!keys->ok) return false;
     if (keys->len <= 0) return false;
@@ -123,7 +169,7 @@ int tomlua_decode(lua_State *L) {
             }
             lua_pop(L, 1);
             if (strict) {
-                if (!heading_nav(L, &keys, false, top)) {
+                if (!heading_nav_strict(L, &keys, false, top)) {
                     free_str_buf(&scratch);
                     clear_keys_result(&keys);
                     lua_pop(L, 1);
@@ -177,7 +223,7 @@ int tomlua_decode(lua_State *L) {
             }
             lua_pop(L, 1);
             if (strict) {
-                if (!heading_nav(L, &keys, false, top)) {
+                if (!heading_nav_strict(L, &keys, false, top)) {
                     free_str_buf(&scratch);
                     clear_keys_result(&keys);
                     lua_pop(L, 1);
