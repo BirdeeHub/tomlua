cc81dfb9fd7dcfbca47f4ffbb8d3b611910d1781
BirdeeHub <birdee@localhost>
2025-09-01 16:24:48 -0700

    Fixed the wobbly chair by adding peanut butter to the wheels
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/tomlua.c

--- src/tomlua.c
+++ src/tomlua.c
@@ -21,40 +21,43 @@ static int tomlua_new(lua_State *L) {
         TomluaUserOpts *opts = lua_newuserdata(L, sizeof(TomluaUserOpts));
         opts->strict = strict;
         opts->enhanced_tables = enhanced_tables;
+
+        // duplicate pointers for encode
+        lua_pushvalue(L, -2);
+        lua_pushvalue(L, -2);
+        lua_pushcclosure(L, tomlua_encode, 2); // pops err+opts, adds function
+        lua_setfield(L, -4, "encode");         // pops function
+
         if (strict) {
             lua_newtable(L);            // create table for defined tables
             lua_newtable(L);            // metatable so we use weak keys
             lua_pushstring(L, "k");
             lua_setfield(L, -2, "__mode");
             lua_setmetatable(L, -2);    // set weak-key table metatable
-            // duplicate pointers for both closures (dont duplicate defined, we don't need it in encode I don't think)
-            lua_pushvalue(L, -3);
-            lua_pushvalue(L, -3);
-            // stack: module, err, opts, defined, err, opts
-            lua_pushcclosure(L, tomlua_encode, 2); // pops err+opts, adds function
-            lua_setfield(L, -5, "encode");         // pops function
             lua_pushcclosure(L, tomlua_decode, 3); // pops err+opts+defined, adds function
             lua_setfield(L, -2, "decode");         // pops function
-            return 1;
+        } else {
+            lua_pushcclosure(L, tomlua_decode, 2); // pops err+opts+defined, adds function
+            lua_setfield(L, -2, "decode");         // pops function
         }
-    } else {
-        lua_settop(L, 0);
-        lua_newtable(L); // module table
-        // upvalue 1: error object
-        new_TMLErr(L);
-        // upvalue 2: options
-        TomluaUserOpts *opts = lua_newuserdata(L, sizeof(TomluaUserOpts));
-        opts->strict = false;
-        opts->enhanced_tables = false;
+        return 1;
     }
-    // duplicate pointers for both closures
+    lua_settop(L, 0);
+    lua_newtable(L); // module table
+    // upvalue 1: error object
+    new_TMLErr(L);
+    // upvalue 2: options
+    TomluaUserOpts *opts = lua_newuserdata(L, sizeof(TomluaUserOpts));
+    opts->strict = false;
+    opts->enhanced_tables = false;
+    // duplicate pointers for encode
     lua_pushvalue(L, -2);
     lua_pushvalue(L, -2);
     // stack: module, err, opts, err, opts
-    lua_pushcclosure(L, tomlua_decode, 2); // pops err+opts, adds function
-    lua_setfield(L, -4, "decode");         // pops function
     lua_pushcclosure(L, tomlua_encode, 2); // pops err+opts, adds function
-    lua_setfield(L, -2, "encode");         // pops function
+    lua_setfield(L, -4, "encode");         // pops function
+    lua_pushcclosure(L, tomlua_decode, 2); // pops err+opts, adds function
+    lua_setfield(L, -2, "decode");         // pops function
     return 1;
 }
 
