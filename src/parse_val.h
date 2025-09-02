b3345cdec03449a600b5f0d7346fa4c70b07ef71
BirdeeHub <birdee@localhost>
2025-09-02 00:11:55 -0700

    Bump the waddle speed of the virtual penguin by 50%
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   bench.lua
    	modified:   src/parse_val.h

--- src/parse_val.h
+++ src/parse_val.h
@@ -11,7 +11,6 @@ bool parse_value(lua_State *L, str_iter *src, str_buf *buf, const TomluaUserOpts
 static inline bool set_kv(lua_State *L, keys_result *keys) {
     if (!keys->ok) return false;
     if (keys->len <= 0) return set_err_upval(L, false, 22, "no key provided to set");
-
     int value_idx = lua_gettop(L);  // value is on top
     int root_idx = value_idx - 1;   // the table to start navigation from
     lua_pushvalue(L, root_idx);     // copy root table to top
@@ -21,17 +20,18 @@ static inline bool set_kv(lua_State *L, keys_result *keys) {
         if (!push_buf_to_lua_string(L, &keys->v[i])) {
             return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
         }
-        lua_gettable(L, -2);    // get t[key]
+        lua_pushvalue(L, -1);
+        lua_gettable(L, -3);    // get t[key]
         if (lua_isnil(L, -1)) {
             lua_pop(L, 1);      // remove nil
             lua_newtable(L);    // create new table
-            if (!push_buf_to_lua_string(L, &keys->v[i])) {
-                return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-            }
-            lua_pushvalue(L, -2);  // push new table
+            lua_pushvalue(L, -1);
+            lua_insert(L, -3);
             lua_settable(L, -4);   // t[key] = new table
         } else if (!lua_istable(L, -1)) {
             return set_err_upval(L, false, 18, "key is not a table");
+        } else {
+            lua_remove(L, -3);
         }
         lua_remove(L, -2); // remove parent table
     }
@@ -100,17 +100,18 @@ static inline bool set_kv_strict(lua_State *L, keys_result *keys) {
         if (!push_buf_to_lua_string(L, &keys->v[i])) {
             return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
         }
-        lua_gettable(L, -2);    // get t[key]
+        lua_pushvalue(L, -1);
+        lua_gettable(L, -3);    // get t[key]
         if (lua_isnil(L, -1)) {
             lua_pop(L, 1);      // remove nil
             lua_newtable(L);    // create new table
-            if (!push_buf_to_lua_string(L, &keys->v[i])) {
-                return set_err_upval(L, false, 48, "tomlua.decode failed to push string to lua stack");
-            }
-            lua_pushvalue(L, -2);  // push new table
+            lua_pushvalue(L, -1);
+            lua_insert(L, -3);
             lua_settable(L, -4);   // t[key] = new table
         } else if (!lua_istable(L, -1)) {
             return set_err_upval(L, false, 18, "key is not a table");
+        } else {
+            lua_remove(L, -3);
         }
         lua_remove(L, -2); // remove parent table
     }
