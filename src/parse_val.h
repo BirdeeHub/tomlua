6b16243dd3302c3bb01bcb16230bc9e7b748ee9a
BirdeeHub <birdee@localhost>
2025-09-01 18:04:30 -0700

    Reorganized the peanut butter and jelly in the Makefile
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/parse_val.h

--- src/parse_val.h
+++ src/parse_val.h
@@ -7,7 +7,7 @@
 
 bool parse_value(lua_State *L, str_iter *src, str_buf *buf, const TomluaUserOpts *opts);
 
-// gets [-1] value and [-2] root table from top of stack but leaves on top of stack, and sets value at place indexed to by keys
+// gets [-1] value and [-2] root table from top of stack but leaves root table on top of stack, and sets value at place indexed to by keys
 static inline bool set_kv(lua_State *L, keys_result *keys) {
     if (!keys->ok) return false;
     if (keys->len <= 0) return set_err_upval(L, false, 22, "no key provided to set");
@@ -104,7 +104,7 @@ static inline bool register_defined(lua_State *L, int idx) {
 }
 
 // TODO: MAKE THIS STRICT
-// gets [-1] value and [-2] root table from top of stack but leaves on top of stack, and sets value at place indexed to by keys
+// gets [-1] value and [-2] root table from top of stack but leaves root table on top of stack, and sets value at place indexed to by keys
 static inline bool set_kv_strict(lua_State *L, keys_result *keys) {
     if (!keys->ok) return false;
     if (keys->len <= 0) return set_err_upval(L, false, 22, "no key provided to set");
