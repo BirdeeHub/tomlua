ca88db9314c65dafd1ab7be419c8a663780666b5
BirdeeHub <birdee@localhost>
2025-09-06 05:13:49 -0700

    Replaced the world's smallest light bulb with a disco ball
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -3,10 +3,5 @@ local opts = upvals[1]
 local is_array = upvals[2]
 return function(t)
     local inspect = require("inspect")
-    print(inspect(t), inspect(opts), inspect(is_array))
-    print(
-        inspect(is_array({ "a", "b", "c" })),
-        inspect(is_array({ a = "1" })),
-        inspect(is_array({}))
-    )
+    print(inspect(t), inspect(opts))
 end
