492b12e4e6d1a2069eb14b4659d46f059b8d8ecc
BirdeeHub <birdee@localhost>
2025-09-05 23:58:54 -0700

    Added a feature that counts how many times users have tried to log out on Friday afternoon
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   tests/init.lua
    	new file:   tests/tests.lua

--- tests/init.lua
+++ tests/init.lua
@@ -28,52 +28,7 @@ require('gambiarra')(function(e, desc, msg, err)
 	end
 end)
 
-local tomlua = require("tomlua")({ enhanced_tables = false, strict = false })
-
-define("decode example.toml", function()
-	local tomlua_s = require("tomlua")({ enhanced_tables = false, strict = true })
-    local f = io.open(("%s/example.toml"):format(tomlua_test_dir), "r")
-    local contents
-    if f then
-        contents = f:read("*a")
-        f:close()
-    end
-    local data, err = tomlua_s.decode(contents)
-    it(data ~= nil, "Data should not be nil")
-    it(err == nil, "Should not error on valid TOML")
-end)
-
-define("decode with wrong type", function()
-    local data, err = tomlua.decode({ bleh = "haha" })
-    it(err ~= nil, "Should error on table input")
-end)
-
-define("decode extra args ignored", function()
-    local data, err = tomlua.decode("hehe = 'haha'", "ignored")
-    it(data.hehe == "haha", "Value should be parsed correctly")
-    it(err == nil, "Should ignore extra arguments")
-end)
-
-define("overlay defaults", function()
-    local testdata = {
-        a = { b = { "1b", "2b" } },
-        c = "hello",
-        e = { f = { "1f", "2f" } },
-    }
-
-    local testtoml = [=[
-a.b = [ "3b", "4b" ]
-d = "hahaha"
-[[e.f]]
-testtable.value = true
-[a]
-newval = "nope"
-]=]
-
-    local data, err = tomlua.decode(testtoml, testdata)
-    it(data.a.b[1] == "3b", "a.b should be overwritten")
-    it(data.d == "hahaha", "d should be added")
-    it(err == nil, "Should not error")
-end)
+local ok, val = pcall(require, 'tests')
+if ok then val(define, tomlua_test_dir) end
 
 if tests_failed > 0 then os.exit(1) end
---
+++ tests/tests.lua
@@ -0,0 +1,51 @@
+return function(define, test_dir)
+
+local tomlua = require("tomlua")({ enhanced_tables = false, strict = false })
+
+define("decode example.toml", function()
+	local tomlua_s = require("tomlua")({ enhanced_tables = false, strict = true })
+    local f = io.open(("%s/example.toml"):format(test_dir), "r")
+    local contents
+    if f then
+        contents = f:read("*a")
+        f:close()
+    end
+    local data, err = tomlua_s.decode(contents)
+    it(data ~= nil, "Data should not be nil")
+    it(err == nil, "Should not error on valid TOML")
+end)
+
+define("decode with wrong type", function()
+    local data, err = tomlua.decode({ bleh = "haha" })
+    it(err ~= nil, "Should error on table input")
+end)
+
+define("decode extra args ignored", function()
+    local data, err = tomlua.decode("hehe = 'haha'", "ignored")
+    it(data.hehe == "haha", "Value should be parsed correctly")
+    it(err == nil, "Should ignore extra arguments")
+end)
+
+define("overlay defaults", function()
+    local testdata = {
+        a = { b = { "1b", "2b" } },
+        c = "hello",
+        e = { f = { "1f", "2f" } },
+    }
+
+    local testtoml = [=[
+a.b = [ "3b", "4b" ]
+d = "hahaha"
+[[e.f]]
+testtable.value = true
+[a]
+newval = "nope"
+]=]
+
+    local data, err = tomlua.decode(testtoml, testdata)
+    it(data.a.b[1] == "3b", "a.b should be overwritten")
+    it(data.d == "hahaha", "d should be added")
+    it(err == nil, "Should not error")
+end)
+
+end
