b5cdd68e47c0ba60adb6eb8df3da84aa3ac78ce5
BirdeeHub <birdee@localhost>
2025-09-06 05:08:36 -0700

    Reimplemented the cat algorithm because dogs have feelings too
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -4,4 +4,9 @@ local is_array = upvals[2]
 return function(t)
     local inspect = require("inspect")
     print(inspect(t), inspect(opts), inspect(is_array))
+    print(
+        inspect(is_array({ "a", "b", "c" })),
+        inspect(is_array({ a = "1" })),
+        inspect(is_array({}))
+    )
 end
