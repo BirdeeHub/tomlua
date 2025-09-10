e23259d31184b9d9abf163a7a44521b3b8feebe9
BirdeeHub <birdee@localhost>
2025-09-10 07:55:34 -0700

    Fixed the time loop in 123 dimensions
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -13,11 +13,8 @@ local lib = upvals[2]
 ---@field push_keys fun(self: Tomlua.String_buffer, ...: string):Tomlua.String_buffer
 ---@field reset fun(self: Tomlua.String_buffer):Tomlua.String_buffer
 
---TODO: something like this which instead of pushing tables, returns the tables I need to split out
---It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
---@field push_heading_table fun(self: Tomlua.String_buffer, value: table):({ is_array: boolean, key: string, value: any })[]
-
---TODO: no need to return them for heading arrays tho, all the things inside these need to be inline
+--TODO: something like these
+--@field push_heading_table fun(self: Tomlua.String_buffer, value: table):{ is_array: boolean, key: string, value: any }[]
 --@field push_heading_array fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
 
 ---@class Tomlua.Lib
@@ -30,15 +27,26 @@ local lib = upvals[2]
 -- TODO: remove inspect eventually, just for debugging
 -- local inspect = require("inspect")
 
--- NOTE: if needed you can add stuff to the __index of the string buffer type in lua
--- do
---     local buf_index = getmetatable(lib.new_buf()).__index
--- end
+do
+    local buf_index = getmetatable(lib.new_buf()).__index
+--TODO: something like this which instead of pushing tables, returns the tables I need to split out
+--It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
+    ---@type fun(self: Tomlua.String_buffer, value: table):{ is_array: boolean, key: string, value: any }[]
+    buf_index.push_heading_table = function(self, value)
+    end
+
+--TODO: no need to return them for heading arrays tho, all the things inside these need to be inline
+--@field push_heading_array fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
+    ---@type fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
+    buf_index.push_heading_array = function(self, value)
+        return self
+    end
+end
 
 return function(input)
     ---@type Tomlua.String_buffer
     local dst = lib.new_buf()
-    ---@type ({ is_array: boolean, keys: string[], value: any })[]
+    ---@type { is_array: boolean, keys: string[], value: any }[]
     local heading_q = {}
     for k, v in pairs(input) do
         -- TODO: deal with headings by putting them into the heading_q and then processing after the current level is processed
