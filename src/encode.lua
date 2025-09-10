103654d1df62fa8ccf9cbdf5b9727244334cee6f
BirdeeHub <birdee@localhost>
2025-09-10 08:18:15 -0700

    Added a feature to make the coffee machine sing "Mocha Java Jingle"
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -8,10 +8,10 @@
 ---@field push_heading fun(self: Tomlua.String_buffer, is_array: boolean, ...: string):Tomlua.String_buffer
 ---@field push_keys fun(self: Tomlua.String_buffer, ...: string):Tomlua.String_buffer
 ---@field reset fun(self: Tomlua.String_buffer):Tomlua.String_buffer
+---@field push_heading_array fun(self: Tomlua.String_buffer, value: table, ...: string):Tomlua.String_buffer
 
 --TODO: something like these, outlined further below
---@field push_heading_table fun(self: Tomlua.String_buffer, value: table):{ is_array: boolean, key: string, value: any }[]
---@field push_heading_array fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
+--@field push_heading_table fun(self: Tomlua.String_buffer, value: table, ...: string):{ is_array: boolean, keys: string[], value: any }[]
 
 ---@class Tomlua.Lib
 ---@field new_buf fun():Tomlua.String_buffer
@@ -28,18 +28,29 @@ local lib = upvals[2]
 -- TODO: remove inspect eventually, just for debugging
 -- local inspect = require("inspect")
 
+---@class Tomlua.Deferred_Heading
+---@field is_array boolean
+---@field keys string[]
+---@field value any
+
 do
     local buf_index = getmetatable(lib.new_buf()).__index
 --TODO: something like this which instead of pushing tables, returns the tables I need to split out
 --It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
-    ---@type fun(self: Tomlua.String_buffer, value: table):{ is_array: boolean, key: string, value: any }[]
-    buf_index.push_heading_table = function(self, value)
+    ---@type fun(self: Tomlua.String_buffer, queue: Tomlua.Deferred_Heading[], value: table, ...: string):Tomlua.Deferred_Heading[]
+    buf_index.push_heading_table = function(self, queue, value, ...)
     end
 
---TODO: no need to return them for heading arrays tho, all the things inside these need to be inline
---@field push_heading_array fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
-    ---@type fun(self: Tomlua.String_buffer, value: table):Tomlua.String_buffer
-    buf_index.push_heading_array = function(self, value)
+    --no need to return them for heading arrays tho, all the things inside these need to be inline
+    --only call it on things checked with is_heading_array
+    ---@type fun(self: Tomlua.String_buffer, value: table, ...: string):Tomlua.String_buffer
+    buf_index.push_heading_array = function(self, value, ...)
+        for _, val in ipairs(value) do
+            self:push_heading(true, ...)
+            for k, v in pairs(val) do
+                self:push_keys(k):push(" = "):push_inline_value(v):push("\n")
+            end
+        end
         return self
     end
 end
@@ -47,7 +58,7 @@ end
 return function(input)
     ---@type Tomlua.String_buffer
     local dst = lib.new_buf()
-    ---@type { is_array: boolean, keys: string[], value: any }[]
+    ---@type Tomlua.Deferred_Heading[]
     local heading_q = {}
     for k, v in pairs(input) do
         -- TODO: deal with headings by putting them into the heading_q and then processing after the current level is processed
