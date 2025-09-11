372ef70ba40d7fae8af796847170a2efb954a8d0
BirdeeHub <birdee@localhost>
2025-09-10 17:19:22 -0700

    Implemented the missing feature that no one knew we needed
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -8,8 +8,6 @@
 ---@field push_inline_value fun(self: Tomlua.String_buffer, visited: table<table, any>, value: any, array_level: number?):Tomlua.String_buffer
 ---@field push_heading fun(self: Tomlua.String_buffer, is_array: boolean, ...: string):Tomlua.String_buffer
 ---@field push_keys fun(self: Tomlua.String_buffer, ...: string):Tomlua.String_buffer
----@field push_heading_array fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string):Tomlua.String_buffer
----@field push_heading_table fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string):Tomlua.Deferred_Heading[]
 
 ---@class Tomlua.Lib
 ---@field new_buf fun():Tomlua.String_buffer
@@ -22,48 +20,44 @@ local opts = upvals[1]
 ---@type Tomlua.Lib
 local lib = upvals[2]
 
-do
-    local buf_index = getmetatable(lib.new_buf()).__index
-    --instead of pushing tables, returns the tables I need to split out
-    --It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
-    ---@type fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string):Tomlua.Deferred_Heading[]
-    buf_index.push_heading_table = function(self, visited, value, ...)
-        ---@type Tomlua.Deferred_Heading[]
-        local result = {}
-        self:push_heading(false, ...)
-        for k, v in pairs(value) do
-            local vtype = type(v)
-            if vtype == "table" then
-                local is_heading_array, is_array = lib.is_heading_array(v)
-                if is_heading_array then
-                    local keys = {...}
-                    table.insert(keys, k)
-                    table.insert(result, { is_array = true, keys = keys, value = v })
-                elseif is_array then
-                    self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
-                else
-                    local keys = {...}
-                    table.insert(keys, k)
-                    table.insert(result, { is_array = false, keys = keys, value = v })
-                end
+--instead of pushing tables, returns the tables I need to split out
+--It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
+---@type fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string):Tomlua.Deferred_Heading[]
+local function push_heading_table(self, visited, value, ...)
+    ---@type Tomlua.Deferred_Heading[]
+    local result = {}
+    self:push_heading(false, ...)
+    for k, v in pairs(value) do
+        local vtype = type(v)
+        if vtype == "table" then
+            local is_heading_array, is_array = lib.is_heading_array(v)
+            if is_heading_array then
+                local keys = {...}
+                table.insert(keys, k)
+                table.insert(result, { is_array = true, keys = keys, value = v })
+            elseif is_array then
+                self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
             else
-                self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
+                local keys = {...}
+                table.insert(keys, k)
+                table.insert(result, { is_array = false, keys = keys, value = v })
             end
+        else
+            self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
         end
-        return result
     end
+    return result
+end
 
-    --no need to return them for heading arrays tho, all the things inside these need to be inline
-    --only call it on things checked with is_heading_array
-    ---@type fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string):Tomlua.String_buffer
-    buf_index.push_heading_array = function(self, visited, value, ...)
-        for _, val in ipairs(value) do
-            self:push_heading(true, ...)
-            for k, v in pairs(val) do
-                self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
-            end
+--no need to return them for heading arrays tho, all the things inside these need to be inline
+--only call it on things checked with is_heading_array
+---@type fun(self: Tomlua.String_buffer, visited: table<table, any>, value: table, ...: string)
+local function push_heading_array(self, visited, value, ...)
+    for _, val in ipairs(value) do
+        self:push_heading(true, ...)
+        for k, v in pairs(val) do
+            self:push_keys(k):push(" = "):push_inline_value(visited, v):push("\n")
         end
-        return self
     end
 end
 
@@ -82,10 +76,9 @@ return function(input)
         while i <= #q do
             local h = q[i]
             if h.is_array then
-                dst:push_heading_array(visited, h.value, unpack(h.keys))
+                push_heading_array(dst, visited, h.value, unpack(h.keys))
             else
-                local more = dst:push_heading_table(visited, h.value, unpack(h.keys))
-                flush_q(more)
+                flush_q(push_heading_table(dst, visited, h.value, unpack(h.keys)))
             end
             i = i + 1
         end
