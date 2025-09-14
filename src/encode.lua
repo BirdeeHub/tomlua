8a71e284c1b707f1e23f836a75414862d877f8c2
BirdeeHub <birdee@localhost>
2025-09-14 02:57:08 -0700

    Added a missing semi-colon after the for-loop declaration in main.c
    (auto-msg qwen2.5-coder:7b)
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   src/encode.lua

--- src/encode.lua
+++ src/encode.lua
@@ -6,8 +6,8 @@
 ---@class Tomlua.String_buffer
 ---@field push fun(self: Tomlua.String_buffer, str: string):Tomlua.String_buffer
 ---@field push_inline_value fun(self: Tomlua.String_buffer, value: any, array_level: number?):Tomlua.String_buffer
----@field push_heading fun(self: Tomlua.String_buffer, is_array: boolean, ...: string):Tomlua.String_buffer
----@field push_keys fun(self: Tomlua.String_buffer, ...: string):Tomlua.String_buffer
+---@field push_heading fun(self: Tomlua.String_buffer, is_array: boolean, keys...: string):Tomlua.String_buffer
+---@field push_keys fun(self: Tomlua.String_buffer, keys...: string):Tomlua.String_buffer
 
 local check_heading_array, new_buf = ...
 
@@ -17,7 +17,7 @@ return function(input)
 
     --instead of pushing tables, returns the tables I need to split out
     --It is to also return them if it is an array of ONLY TABLES, otherwise it prints the array inline
-    ---@type fun(value: table, ...: string):Tomlua.Deferred_Heading[]
+    ---@type fun(value: table, keys...: string):Tomlua.Deferred_Heading[]
     local function push_heading_table(value, ...)
         ---@type Tomlua.Deferred_Heading[]
         local result = {}
