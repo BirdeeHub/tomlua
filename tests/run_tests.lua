d691ec4b47e2606189fdb978d915656aefea700c
BirdeeHub <birdee@localhost>
2025-09-25 04:46:39 -0700

    refactor(whitespace): added a newline

--- tests/run_tests.lua
+++ tests/run_tests.lua
@@ -2,6 +2,7 @@ local cfg = string.gmatch(package.config, "(%S+)")
 local dirsep, pathsep, pathmark = cfg() or "/", cfg() or ";", cfg() or "?"
 local tomlua_test_dir = debug.getinfo(1, 'S').source:sub(2):match("(.*)"..dirsep..".-") or "."
 package.path = ("%s%s%s.lua%s"):format(tomlua_test_dir, dirsep, pathmark, pathsep) .. package.path
+
 package.cpath = ("%s%s%s.so%s"):format(arg[1], dirsep, pathmark, pathsep) .. package.cpath
 
 local define = require('gambiarra')
